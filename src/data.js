ccf6b8226c74e774f4a1801de6b47d99585aa7e5
Krupa Sri <srinivasankrupa7@gmail.com>
2023-10-14 21:36:13 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,44 @@
+export const projects = [
+  {
+    title: "Homepage Redesign",
+    subtitle: "Bootstrap",
+    description:
+      "Discover a responsive, sleek, and interactive Bootstrap-powered homepage. This project showcases modern UI, optimized performance, and accessibility, offering a seamless web experience.",
+    image: "./p1.png",
+    
+  },
+  {
+    title: "Calculator App",
+    subtitle: "React JS ",
+    description:"Discover a feature-rich calculator app built with React.js. Enjoy a responsive and user-friendly interface, supporting basic and advanced mathematical operations, and a clean, modern design for effortless calculations.",
+      
+    image: "./p3.png",
+    
+  },
+  {
+    title: "Responsive Login Page",
+    subtitle: "Javascript",
+    description:
+      "Unlock a secure and intuitive login experience with this JavaScript-driven login page. Featuring robust form validation, seamless error handling, and an interactive design, it ensures a modern and reliable user authentication process",
+    image: "./p2.png",
+    
+  },
+  {
+    title: "Color Picker App",
+    subtitle: "React JS ",
+    description:
+      "Discover a React-powered Color Picker app for effortless color selection. Intuitively customize and save colors with real-time previews, streamlining your design process",
+    image: "./p4.png",
+    
+  },
+];
+
+
+export const skills = [
+  "Python",
+  "JavaScript",
+  "HTML",
+  "React",
+  "C++",
+  "Bootstrap",
+];
